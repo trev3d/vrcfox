ddd6c13a08280defbcc805eafdd5784931ea0adb
Julian Triveri <triveri@outlook.com>
2023-04-09 15:57:38 -0400

    add big PC/Quest label to scene view so I don't forget which scene I'm in when uploading

---
+++ vrcfox unity project/Assets/scripts/SceneLabel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[InitializeOnLoad]
+public class SceneLabel
+{
+	private static GUIStyle style= new GUIStyle();
+
+	static SceneLabel()
+	{
+		style.normal.textColor = Color.yellow;
+		style.fontSize = 50;
+		style.fontStyle = FontStyle.Bold;
+		style.alignment = TextAnchor.MiddleCenter;
+		SceneView.duringSceneGui += OnScene;
+	}
+
+	private static void OnScene(SceneView sceneview)
+	{
+		Handles.BeginGUI();
+		GUILayout.BeginArea(new Rect(0, 0, sceneview.camera.pixelWidth, 100));
+		GUILayout.Label(SceneManager.GetActiveScene().name, style);
+		GUILayout.EndArea();
+
+		Handles.EndGUI();
+	}
+}
